8e3ddd17e4b32ed54a303f589551a30abd0cbe00
Sai Coumar <55699636+saiccoumar@users.noreply.github.com>
2021-10-24 12:24:54 -0400

    fourth commit

--- frontend/index.js
+++ frontend/index.js
@@ -5,7 +5,6 @@ var express = require("express"),
     request = require('request'),
     views = __dirname + "/views/",
     mongoSanitize = require('express-mongo-sanitize'),
-    https = require('https'),
     url = "mongodb+srv://ExternalUser:7ZKqWRIRgvP4tF4I@recommendationrulesamer.a7xea.mongodb.net/anime?retryWrites=true&w=majority";
 
 
@@ -15,7 +14,10 @@ mongoose.connect(url, {
 })
 
 app.set('view engine', 'ejs');
-app.use(express.static(__dirname + "style"));
+console.log("dirname: "+__dirname+ "/static");
+
+app.use(express.static(__dirname));
+
 app.use(mongoSanitize());
 
 
@@ -51,11 +53,66 @@ app.get("/", function (req, res) {
         dummy = body;
         console.log("Dummy: "+dummy);
         // console.log("Dummy2: "+dummy);
-        res.render("index.ejs",{ dum: dummy});
+        res.render("index.ejs",{ dum: dummy, q: ""});
         // res.send(body); //Display the response on the website
     });
     
 });
+
+
+app.get("/search", function (req, res) {
+  noMatch = "",
+  dataTotal = []
+  // console.log(req.query.sorted);
+  if (req.query.search.length > 1) {
+
+      const regex = new RegExp(escapeRegex(req.query.search), 'gi');
+      // Get all campgrounds from DB
+      Anime.find({
+          Left: {
+              $regex: regex
+              // $regex: "left3"
+          }
+      }, {
+          _id: 0,
+          __v: 0
+      }, function (error, data) {
+          if (error) {
+              console.log(err);
+          } else {
+
+              data.forEach(x => {
+                  //console.log(dataTotal);
+                  // x["Confidence"] = x["Confidence"].substring(0, 6);
+                  dataTotal.push(x);
+              });
+              //console.log(data);
+          }
+
+          // console.log(dataTotal);
+          res.render("index.ejs", { data: sortResults(dataTotal, req.query.sorted), noMatch: noMatch, sort: sort, q: req.query.search, q2: req.query.sorted });
+      });
+
+
+
+  } else {
+      // Get all animes from DB
+      Anime.find({}, function (err, data) {
+          if (err) {
+              console.log(err);
+          } else {
+              res.render("index.ejs", { data: sortResults(dataTotal), noMatch: noMatch, sort: sort, q: "", q2: "None" });
+          }
+      });
+  }
+});
+
+function escapeRegex(text) {
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/, "\\$&");
+};
+
+
+
 
 app.listen(port, function () {
     console.log("Server Started")
